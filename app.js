b21b640f1f6fc8bf5e5ba4b51503111ef877ed7a
Adjei-Kuranchie <yaw.kuranchie@stu.ucc.edu.gh>
2025-07-01 15:26:24 +0000

    feat: enhance error handling middleware and update app.js structure

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 import { PORT } from "./config/env.js";
 import connectDB from "./db/connect.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 import { authRouter, subscriptionRouter, userRouter } from "./routes/index.js";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
 
 // Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
-// Basic route
+app.use(errorMiddleware);
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the API!" });
 });
